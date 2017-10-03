0193df42b5dae4a194d4e98cb09a0b4ecd8d9a90
Mark Liang <mliang@mozilla.com>
2017-10-03 14:02:48 +0800

    add modal

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom';
 
 class Home extends Component {
   componentDidMount() {
-    document.title = "Home";
+    document.title = "Za-Tone";
   }
   render() {
     return (
       <div>
         <header className="min-vh-100 pv5 df">
         	<div className="center w-100 mw8 ph3 ph5-ns">
-            <figure className="w-100 h5 bg-near-white mh2 mb4"></figure>
+            <figure className="w-100 h5 bg-near-white mh0 mb4"></figure>
             <h1 className="tc">This is a header</h1>
             <h3 className="tc">This is a description</h3>
           </div>
         </header>
         <section className="bg-dark-gray">
-          <div className="center w-100 mw8 ph3 pv4 tc">
+          <div className="center w-100 mw8 ph3 pv5 tc">
             <h2 className="white">This is a banner title</h2>
             <h4 className="white">This is a banner description</h4>
           </div>
@@ -25,46 +25,46 @@ class Home extends Component {
         	<div className="center w-100 mw8 ph3 ph5-ns tc mb5">
             <h2>This is a section title</h2>
             <h4>This is a section description</h4>
-            <div class="mw9 center mt5">
-              <div class="cf">
-                <div class="fl w-100 w-third-l w-100 pa2">
+            <div className="mw9 center mt5">
+              <div className="cf">
+                <div className="fl w-100 w-third-l w-100 pa2">
                   <Link to='/self-exploration'>
-                    <div class="bg-white pv4 h5 df">
+                    <div className="bg-white pv4 h5 df">
                       <h3>自我認識</h3>
                     </div>
                   </Link>
                 </div>
-                <div class="fl w-100 w-third-l w-100 pa2">
+                <div className="fl w-100 w-third-l w-100 pa2">
                   <Link to='/gender-equality'>
-                    <div class="bg-white pv4 h5 df">
+                    <div className="bg-white pv4 h5 df">
                       <h3>性別平等</h3>
                     </div>
                   </Link>
                 </div>
-                <div class="fl w-100 w-third-l w-100 pa2">
+                <div className="fl w-100 w-third-l w-100 pa2">
                   <Link to='/aesthetic-education'>
-                    <div class="bg-white pv4 h5 df">
+                    <div className="bg-white pv4 h5 df">
                       <h3>美感教育</h3>
                     </div>
                   </Link>
                 </div>
-                <div class="fl w-100 w-third-l w-100 pa2">
+                <div className="fl w-100 w-third-l w-100 pa2">
                   <Link to='/pursue-your-dreams'>
-                    <div class="bg-white pv4 h5 df">
+                    <div className="bg-white pv4 h5 df">
                       <h3>追求夢想</h3>
                     </div>
                   </Link>
                 </div>
-                <div class="fl w-100 w-third-l w-100 pa2">
+                <div className="fl w-100 w-third-l w-100 pa2">
                   <Link to='/independent-thinking'>
-                    <div class="bg-white pv4 h5 df">
+                    <div className="bg-white pv4 h5 df">
                       <h3>獨立思考</h3>
                     </div>
                   </Link>
                 </div>
-                <div class="fl w-100 w-third-l w-100 pa2">
+                <div className="fl w-100 w-third-l w-100 pa2">
                   <Link to='/'>
-                    <div class="bg-white pv4 h5 df">
+                    <div className="bg-white pv4 h5 df">
                       <h3>?</h3>
                     </div>
                   </Link>
--- src/components/pages/Topic1.js
+++ src/components/pages/Topic1.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import FontAwesome from 'react-fontawesome';
 import Swiper from 'swiper';
+import Modal from 'react-responsive-modal';
+
+var modalId;
 
 class TopicA extends Component {
   componentDidMount() {
@@ -9,8 +12,8 @@ class TopicA extends Component {
     var swiper = new Swiper('.swiper-container', {
       pagination: '.swiper-pagination',
       slidesPerView: 3,
+      simulateTouch: false,
       paginationClickable: true,
-      loop: true,
       nextButton: '.swiper-button-next',
       prevButton: '.swiper-button-prev',
       spaceBetween: 0,
@@ -22,14 +25,56 @@ class TopicA extends Component {
         }
       }
     });
+  };
+
+  state = {
+    open: false,
+  };
+
+  onOpenModal = (e) => {
+    modalId = e.target.dataset.id;
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  modalContent = (a) => {
+    switch(a) {
+      case "1":
+        return (
+          <h1>1</h1>
+        );
+      case "2":
+        return (
+          <h1>2</h1>
+        );
+      case "3":
+        return (
+          <h1>3</h1>
+        );
+      case "4":
+        return (
+          <h1>4</h1>
+        );
+      case "5":
+        return (
+          <h1>5</h1>
+        );
+      default:
+        break;
+    } 
   }
+
   render() {
+    const { open } = this.state;
     return (
       <div>
         {/*--- Header ---*/}
         <header className="min-vh-100 pv5 df">
           <div className="center w-100 mw8 ph3 ph5-ns">
-            <figure className="w-100 h5 bg-near-white mh2 mb4"></figure>
+            <figure className="w-100 h5 bg-near-white mh0 mb4"></figure>
             <h1 className="tc">自我認識 <FontAwesome name='rocket'/></h1>
             <h3 className="tc">This is a description</h3>
           </div>
@@ -75,17 +120,32 @@ class TopicA extends Component {
             <h4>This is a section description</h4>
             <div className="swiper-container mt5 mh2">
               <div className="swiper-wrapper h5">
-                <div className="swiper-slide bg-near-white df">Slide 1</div>
-                <div className="swiper-slide bg-near-white df">Slide 2</div>
-                <div className="swiper-slide bg-near-white df">Slide 3</div>
-                <div className="swiper-slide bg-near-white df">Slide 4</div>
-                <div className="swiper-slide bg-near-white df">Slide 5</div>
+                <div className="swiper-slide bg-near-white df cp" data-id="1" onClick={this.onOpenModal}>
+                  Slide 1
+                </div>
+                <div className="swiper-slide bg-near-white df cp" data-id="2" onClick={this.onOpenModal}>
+                  Slide 2
+                </div>
+                <div className="swiper-slide bg-near-white df cp" data-id="3" onClick={this.onOpenModal}>
+                  Slide 3
+                </div>
+                <div className="swiper-slide bg-near-white df cp" data-id="4" onClick={this.onOpenModal}>
+                  Slide 4
+                </div>
+                <div className="swiper-slide bg-near-white df cp" data-id="5" onClick={this.onOpenModal}>
+                  Slide 5
+                </div>
               </div>
             </div>
             <div className="swiper-pagination mt3"></div>
             <div className="swiper-button-next"></div>
             <div className="swiper-button-prev"></div>
           </div>
+          <Modal open={open} onClose={this.onCloseModal} little>
+            <div className="mw5">
+              {this.modalContent(modalId)}
+            </div>
+          </Modal>
         </section>
         {/*--- Section 3 ---*/}
         <section className="bg-near-white pv5">
@@ -95,46 +155,29 @@ class TopicA extends Component {
             <div className="mw9 center mt5">
               <div className="cf">
                 <div className="fl w-100 w-third-l w-100 pa2">
-                  <Link to='/self-exploration'>
-                    <div className="bg-white pv4 h5 df">
-                      <h3>自我認識</h3>
-                    </div>
-                  </Link>
+                  <div className="bg-white pv4 h5 df cp" data-id="1" onClick={this.onOpenModal}>
+                    <h3 className="pn">1</h3>
+                  </div>
                 </div>
                 <div className="fl w-100 w-third-l w-100 pa2">
-                  <Link to='/gender-equality'>
-                    <div className="bg-white pv4 h5 df">
-                      <h3>性別平等</h3>
-                    </div>
-                  </Link>
+                  <div className="bg-white pv4 h5 df cp" data-id="2" onClick={this.onOpenModal}>
+                    <h3 className="pn">2</h3>
+                  </div>
                 </div>
                 <div className="fl w-100 w-third-l w-100 pa2">
-                  <Link to='/aesthetic-education'>
-                    <div className="bg-white pv4 h5 df">
-                      <h3>美感教育</h3>
-                    </div>
-                  </Link>
+                  <div className="bg-white pv4 h5 df cp" data-id="3" onClick={this.onOpenModal}>
+                    <h3 className="pn">3</h3>
+                  </div>
                 </div>
                 <div className="fl w-100 w-third-l w-100 pa2">
-                  <Link to='/pursue-your-dreams'>
-                    <div className="bg-white pv4 h5 df">
-                      <h3>追求夢想</h3>
-                    </div>
-                  </Link>
+                  <div className="bg-white pv4 h5 df cp" data-id="4" onClick={this.onOpenModal}>
+                    <h3 className="pn">4</h3>
+                  </div>
                 </div>
                 <div className="fl w-100 w-third-l w-100 pa2">
-                  <Link to='/independent-thinking'>
-                    <div className="bg-white pv4 h5 df">
-                      <h3>獨立思考</h3>
-                    </div>
-                  </Link>
-                </div>
-                <div className="fl w-100 w-third-l w-100 pa2">
-                  <Link to='/'>
-                    <div className="bg-white pv4 h5 df">
-                      <h3>?</h3>
-                    </div>
-                  </Link>
+                  <div className="bg-white pv4 h5 df cp" data-id="5" onClick={this.onOpenModal}>
+                    <h3 className="pn">5</h3>
+                  </div>
                 </div>
               </div>
             </div>
