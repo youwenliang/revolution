afcf7f410b762bd464d5c568e9ffd5f4bbf4fc99
Mark Liang <big.head.cool@gmail.com>
2017-10-14 12:09:16 +0800

    add exhibit

--- src/components/topic/Topic4.jsx
+++ src/components/topic/Topic4.jsx
@@ -45,49 +45,56 @@ var modalString = {
                       '介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹介紹'
                      ],
   'exhibit-1-image' :['images/'+pageURL+'/exhibit_1_cover.jpg', 'images/'+pageURL+'/exhibit_1_logo.jpg'],
-  'exhibit-1'       :['【臺灣吧】用動畫說故事，新媒體的教育革命', 
+  'exhibit-1'       :['臺灣吧',
+                      '【臺灣吧】用動畫說故事，新媒體的教育革命', 
                       '臺灣吧是自產內容的新媒體公司，目標是『讓臺灣成為全球數位內容的燈塔』。我們試著以優質的數位內容，替臺灣找到下一個不可取代的理由， 並努力解決教育、傳播與娛樂等產業正在面臨的困境。',
                       '相關連結', 
                       'http://www.taiwanbar.cc/',
                       'https://www.facebook.com/taiwanbarstudio/'
                      ],
-  'exhibit-2-image' :['images/1920x1080.png', 'images/400x400.png'],
-  'exhibit-2'       :['【臺灣吧】用動畫說故事，新媒體的教育革命', 
+  'exhibit-2-image' :['images/'+pageURL+'/exhibit_2_cover.jpg', 'images/'+pageURL+'/exhibit_2_logo.jpg'],
+  'exhibit-2'       :['宜蘭人文國中小士林行動教室',
+                      '【宜蘭人文中小】適性教育，一起無框架學習', 
+                      '以開展天賦為教育核心，實踐「生活即教育、社會即學校」，運用行動學習模式，並以經驗知識取代套裝知識，搭配多元智能理論，再配合每個孩子的發展階段與興趣，彈性調整課程內容與進行方式。以達到適性化、個別化，充分開展學生天賦的理想。',
+                      '相關連結', 
+                      'http://www.explorationalclassroom.com/',
+                      'https://www.facebook.com/jwpsnews/'
+                     ],
+  'exhibit-3-image' :['images/'+pageURL+'/exhibit_3_cover.jpg', 'images/'+pageURL+'/exhibit_3_logo.jpg'],
+  'exhibit-3'       :['青醒',
+                      '【青醒】青年覺醒，點燃一場學習的革命', 
+                      '《Awakening青醒》是一個以行動為導向的國際草根媒體平台，以「青年探索與行動的攻略」自許，取名自「青年覺醒」。青年覺醒，過程即是目的：從改變自己的學習歷程開始，Awakening試圖點燃一場學習革命，啟動華人教育的典範轉移。',
+                      '相關連結', 
+                      'http://www.awakeningedu.org/',
+                      'https://www.facebook.com/Awakening.edu/'
+                     ],
+  'exhibit-4-image' :['images/'+pageURL+'/exhibit_4_cover.jpg', 'images/'+pageURL+'/exhibit_4_logo.jpg'],
+  'exhibit-4'       :['臺灣吧',
+                      '【臺灣吧】用動畫說故事，新媒體的教育革命', 
                       '臺灣吧是自產內容的新媒體公司，目標是『讓臺灣成為全球數位內容的燈塔』。我們試著以優質的數位內容，替臺灣找到下一個不可取代的理由， 並努力解決教育、傳播與娛樂等產業正在面臨的困境。',
                       '相關連結', 
                       'http://www.taiwanbar.cc/',
                       'https://www.facebook.com/taiwanbarstudio/'
                      ],
-  'exhibit-3-image' :['images/1920x1080.png', 'images/400x400.png'],
-  'exhibit-3'       :['【臺灣吧】用動畫說故事，新媒體的教育革命', 
+  'exhibit-5-image' :['images/'+pageURL+'/exhibit_5_cover.jpg', 'images/'+pageURL+'/exhibit_5_logo.jpg'],
+  'exhibit-5'       :['臺灣吧',
+                      '【臺灣吧】用動畫說故事，新媒體的教育革命', 
                       '臺灣吧是自產內容的新媒體公司，目標是『讓臺灣成為全球數位內容的燈塔』。我們試著以優質的數位內容，替臺灣找到下一個不可取代的理由， 並努力解決教育、傳播與娛樂等產業正在面臨的困境。',
                       '相關連結', 
                       'http://www.taiwanbar.cc/',
                       'https://www.facebook.com/taiwanbarstudio/'
                      ],
-  'exhibit-4-image' :['images/1920x1080.png', 'images/400x400.png'],
-  'exhibit-4'       :['【臺灣吧】用動畫說故事，新媒體的教育革命', 
+  'exhibit-6-image' :['images/'+pageURL+'/exhibit_6_cover.jpg', 'images/'+pageURL+'/exhibit_6_logo.jpg'],
+  'exhibit-6'       :['臺灣吧',
+                      '【臺灣吧】用動畫說故事，新媒體的教育革命', 
                       '臺灣吧是自產內容的新媒體公司，目標是『讓臺灣成為全球數位內容的燈塔』。我們試著以優質的數位內容，替臺灣找到下一個不可取代的理由， 並努力解決教育、傳播與娛樂等產業正在面臨的困境。',
                       '相關連結', 
                       'http://www.taiwanbar.cc/',
                       'https://www.facebook.com/taiwanbarstudio/'
                      ],
-  'exhibit-5-image' :['images/1920x1080.png', 'images/400x400.png'],
-  'exhibit-5'       :['【臺灣吧】用動畫說故事，新媒體的教育革命', 
-                      '臺灣吧是自產內容的新媒體公司，目標是『讓臺灣成為全球數位內容的燈塔』。我們試著以優質的數位內容，替臺灣找到下一個不可取代的理由， 並努力解決教育、傳播與娛樂等產業正在面臨的困境。',
-                      '相關連結', 
-                      'http://www.taiwanbar.cc/',
-                      'https://www.facebook.com/taiwanbarstudio/'
-                     ],
-  'exhibit-6-image' :['images/1920x1080.png', 'images/400x400.png'],
-  'exhibit-6'       :['【臺灣吧】用動畫說故事，新媒體的教育革命', 
-                      '臺灣吧是自產內容的新媒體公司，目標是『讓臺灣成為全球數位內容的燈塔』。我們試著以優質的數位內容，替臺灣找到下一個不可取代的理由， 並努力解決教育、傳播與娛樂等產業正在面臨的困境。',
-                      '相關連結', 
-                      'http://www.taiwanbar.cc/',
-                      'https://www.facebook.com/taiwanbarstudio/'
-                     ],
-  'exhibit-7-image' :['images/1920x1080.png', 'images/400x400.png'],
-  'exhibit-7'       :['【臺灣吧】用動畫說故事，新媒體的教育革命', 
+  'exhibit-7-image' :['images/'+pageURL+'/exhibit_7_cover.jpg', 'images/'+pageURL+'/exhibit_7_logo.jpg'],
+  'exhibit-7'       :['臺灣吧',
+                      '【臺灣吧】用動畫說故事，新媒體的教育革命', 
                       '臺灣吧是自產內容的新媒體公司，目標是『讓臺灣成為全球數位內容的燈塔』。我們試著以優質的數位內容，替臺灣找到下一個不可取代的理由， 並努力解決教育、傳播與娛樂等產業正在面臨的困境。',
                       '相關連結', 
                       'http://www.taiwanbar.cc/',
@@ -121,10 +128,15 @@ class TopicD extends Component {
 
     /* Preload Image */
     var images  = [];
-    images.push('images/1920x1080.png');
-    images.push('images/600x600.png');
-    images.push('images/1024x768.png');
-    images.push('images/small_1024x768.png');
+    for ( var i = 1; i <= 6; i++) {
+      images.push('images/'+pageURL+'/雜ＸTONE_icon-0'+i+'.png');
+    }
+    for ( var j = 1; i <= 3; j++) {
+      images.push('images/'+pageURL+'/exhibit_'+j+'_cover.jpg');
+      images.push('images/'+pageURL+'/exhibit_'+j+'_logo.jpg');
+    }
+    images.push('images/'+pageURL+'/cover_video_poster.png');
+    images.push('images/'+pageURL+'/cover_video_poster_small.png');
 
     loadImage(images)
     .then(function (allImgs) {
@@ -235,15 +247,15 @@ class TopicD extends Component {
               </figure>
             </div>
             <div className="o2 w-100 w-70-l tl-l tc df dfc pl4-l pl0 mw6">
-              <h2 className="w-100 fw5 f35 tc tl-l mt3 mt0-l">{modalString[a][0].split('】')[0].replace('【','')}</h2>
-              <h4 className="w-100 mt2 tl">{modalString[a][1]}</h4>
+              <h2 className="w-100 fw5 f35 tc tl-l mt3 mt0-l">{modalString[a][0]}</h2>
+              <h4 className="w-100 mt2 tl">{modalString[a][2]}</h4>
             </div>
           </div>
           <hr/>
-          <h3 className="w-100 fw5 mt1 mb4">{modalString[a][2]}</h3>
+          <h3 className="w-100 fw5 mt1 mb4">{modalString[a][3]}</h3>
           <div className="mb3">
-            <p className="w-100 mt2 tl"><span className="nowrap">官網：</span><a href={modalString[a][3]} target="_blank">{modalString[a][3]}</a></p>
-            <p className="w-100 mt2 tl"><span className="nowrap">粉專：</span><a href={modalString[a][4]} target="_blank">{modalString[a][4]}</a></p>
+            <p className="w-100 mt2 tl"><span className="nowrap">官網：</span><a href={modalString[a][4]} target="_blank">{modalString[a][4]}</a></p>
+            <p className="w-100 mt2 tl"><span className="nowrap">粉專：</span><a href={modalString[a][5]} target="_blank">{modalString[a][5]}</a></p>
           </div>
         </div>
       );
@@ -404,72 +416,72 @@ class TopicD extends Component {
                   <div className="pb4 cp center content-block bg-white ba b--moon-gray" data-id="exhibit-1" onClick={this.onOpenModal}>
                     <figure className="db center w-100 pn topImg">
                       <img src={'images/'+pageURL+'/exhibit_1_cover.jpg'} alt=""/>
-                      <h3 className="absolute white">臺灣吧</h3>
+                      <h3 className="absolute white">{modalString['exhibit-1'][0]}</h3>
                     </figure>
-                    <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-1'][0]}</h3>
-                    <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-1'][1].substring(0, 45)}...</p>
+                    <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-1'][1]}</h3>
+                    <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-1'][2].substring(0, 45)}...</p>
                   </div>
                 </div>
                 <div className="fl w-100 w-third-l pa2 hideme hidediv mb3 mb0-l">
                   <div className="pb4 cp center content-block bg-white ba b--moon-gray" data-id="exhibit-2" onClick={this.onOpenModal}>
                     <figure className="db center w-100 pn topImg">
-                      <img src="images/1920x1080.png" alt=""/>
-                      <h3 className="absolute white">測試文字</h3>
+                      <img src={'images/'+pageURL+'/exhibit_2_cover.jpg'} alt=""/>
+                      <h3 className="absolute white">{modalString['exhibit-2'][0]}</h3>
                     </figure>
-                    <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-2'][0]}</h3>
-                    <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-2'][1].substring(0, 45)}...</p>
+                    <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-2'][1]}</h3>
+                    <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-2'][2].substring(0, 45)}...</p>
                   </div>
                 </div>
                 <div className="fl w-100 w-third-l pa2 hideme hidediv mb3 mb0-l">
                   <div className="pb4 cp center content-block bg-white ba b--moon-gray" data-id="exhibit-3" onClick={this.onOpenModal}>
                     <figure className="db center w-100 pn topImg">
-                      <img src="images/1920x1080.png" alt=""/>
-                      <h3 className="absolute white">測試文字</h3>
+                      <img src={'images/'+pageURL+'/exhibit_3_cover.jpg'} alt=""/>
+                      <h3 className="absolute white">{modalString['exhibit-3'][0]}</h3>
                     </figure>
-                    <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-3'][0]}</h3>
-                    <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-3'][1].substring(0, 45)}...</p>
+                    <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-3'][1]}</h3>
+                    <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-3'][2].substring(0, 45)}...</p>
                   </div>
                 </div>
                 <div className="hide" id="more_2">
                   <div className="fl w-100 w-third-l pa2 hideme hidediv mb3 mb0-l">
                     <div className="pb4 cp center content-block bg-white ba b--moon-gray" data-id="exhibit-4" onClick={this.onOpenModal}>
                       <figure className="db center w-100 pn topImg">
-                        <img src="images/1920x1080.png" alt=""/>
-                        <h3 className="absolute white">測試文字</h3>
+                        <img src={'images/'+pageURL+'/exhibit_4_cover.jpg'} alt=""/>
+                        <h3 className="absolute white">{modalString['exhibit-4'][0]}</h3>
                       </figure>
-                      <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-4'][0]}</h3>
-                      <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-4'][1].substring(0, 45)}...</p>
+                      <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-4'][1]}</h3>
+                      <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-4'][2].substring(0, 45)}...</p>
                     </div>
                   </div>
                   <div className="fl w-100 w-third-l pa2 hideme hidediv mb3 mb0-l">
                     <div className="pb4 cp center content-block bg-white ba b--moon-gray" data-id="exhibit-5" onClick={this.onOpenModal}>
                       <figure className="db center w-100 pn topImg">
-                        <img src="images/1920x1080.png" alt=""/>
-                        <h3 className="absolute white">測試文字</h3>
+                        <img src={'images/'+pageURL+'/exhibit_5_cover.jpg'} alt=""/>
+                        <h3 className="absolute white">{modalString['exhibit-5'][0]}</h3>
                       </figure>
-                      <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-5'][0]}</h3>
-                      <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-5'][1].substring(0, 45)}...</p>
+                      <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-5'][1]}</h3>
+                      <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-5'][2].substring(0, 45)}...</p>
                     </div>
                   </div>
                   <div className="fl w-100 w-third-l pa2 hideme hidediv mb3 mb0-l">
                     <div className="pb4 cp center content-block bg-white ba b--moon-gray" data-id="exhibit-6" onClick={this.onOpenModal}>
                       <figure className="db center w-100 pn topImg">
-                        <img src="images/1920x1080.png" alt=""/>
-                        <h3 className="absolute white">測試文字</h3>
+                        <img src={'images/'+pageURL+'/exhibit_6_cover.jpg'} alt=""/>
+                        <h3 className="absolute white">{modalString['exhibit-6'][0]}</h3>
                       </figure>
-                      <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-6'][0]}</h3>
-                      <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-6'][1].substring(0, 45)}...</p>
+                      <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-6'][1]}</h3>
+                      <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-6'][2].substring(0, 45)}...</p>
                     </div>
                   </div>
                   <div className="hide" id="more_1">
                     <div className="fl w-100 w-third-l pa2 hideme hidediv mb3 mb0-l">
                       <div className="pb4 cp center content-block bg-white ba b--moon-gray" data-id="exhibit-7" onClick={this.onOpenModal}>
                         <figure className="db center w-100 pn topImg">
-                          <img src="images/1920x1080.png" alt=""/>
-                          <h3 className="absolute white">測試文字</h3>
+                          <img src={'images/'+pageURL+'/exhibit_7_cover.jpg'} alt=""/>
+                          <h3 className="absolute white">{modalString['exhibit-7'][0]}</h3>
                         </figure>
-                        <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-7'][0]}</h3>
-                        <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-7'][1].substring(0, 45)}...</p>
+                        <h3 className="center tl mt3 pn mh3 fw5 ph3">{modalString['exhibit-7'][1]}</h3>
+                        <p className="f18 center tl pn mt2 ph3">{modalString['exhibit-7'][2].substring(0, 45)}...</p>
                       </div>
                     </div>
                   </div>
